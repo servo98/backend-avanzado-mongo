55beb6b6c25498c1108911e724bc022d5da3ccdc
servo pcerda <fernandonando98@live.com.mx>
2022-07-04 21:28:18 -0500

    feat: Database connection and Book model creation

---
+++ app.js
@@ -0,0 +1,13 @@
+const { mongoose } = require('./database');
+
+const Book = mongoose.model('Book', {
+  author: String,
+  title: String,
+});
+
+const newBook = new Book({
+  title: 'El mejor libro del mundo',
+  author: 'El mejor autor del libro',
+});
+
+newBook.save().then(console.log).catch(console.error);
