f2d74df8399a6ae4432dcd1af3824ee09578c6a3
servo pcerda <fernandonando98@live.com.mx>
2022-07-06 19:31:05 -0500

    feat: add db events

--- config/database.js
+++ config/database.js
@@ -1,6 +1,28 @@
 import mongoose from 'mongoose';
 import config from './index.js';
 
+const db = mongoose.connection;
+
+db.on('connecting', () => {
+  console.log('Intentando conectar a la base de datos 🙄');
+});
+
+db.on('error', () => {
+  console.error('💀 👺Ha ocurrido un error en la conexión de la base de datos');
+});
+
+db.on('connected', () => {
+  console.log('🟩 Conexión establecida con la base de datos');
+});
+
+db.on('disconnected', () => {
+  console.log('😫 Se ha desconectado la base de datos');
+});
+
+db.on('reconnected', () => {
+  console.log('Se ha reconectado la base 🤗');
+});
+
 export default () => {
   mongoose.connect(`${config.database.uri}/${config.database.name}`);
 };
